d38c7f2e73eed634d85b745837cacf40f71b5e08
Кобыжев Александр <kam-2000@mail.ru>
2018-11-25 20:23:24 +0300

    Начало lesson 8

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -76,7 +76,8 @@ data class Circle(val center: Point, val radius: Double) {
      * расстояние между их центрами минус сумма их радиусов.
      * Расстояние между пересекающимися окружностями считать равным 0.0.
      */
-    fun distance(other: Circle): Double = TODO()
+    fun distance(other: Circle): Double = if (center.distance(other.center) > radius + other.radius)
+        center.distance(other.center) - radius - other.radius else 0.0
 
     /**
      * Тривиальная
